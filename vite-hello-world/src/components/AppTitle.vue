c0287d328957a8df3512d6593f388fbf94ad7661
LucaMarchesani <lucamarchesani.lavoro@gmail.com>
2022-12-27 03:19:56 +0100

    vite component

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    margin: 1rem 1rem;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    color: #d6001c;
+    text-shadow: 5px 5px 2px rgba(0, 0, 0, 0.7);
+    font-size: 3.3rem;
+}
+</style>
